cb56a8591ba38971d9c13661a2a35cd6a225949d
Drishti Samvedi <89981296+Drishti1104@users.noreply.github.com>
2021-09-16 01:23:50 +0530

    Create Employees.java
    Program that prints information of employees

---
+++ Employees.java
@@ -0,0 +1,12 @@
+package com.company;
+
+
+public class Employee {
+    public static void main(String[] args) {
+        System.out.println("Name Year of Joining Address");
+        System.out.println("Robert 1994 64C-WallsStreet");
+        System.out.println("Sam 2000 68D-WallsStreet");
+        System.out.println("John 1999 26B-WallsStreet");
+
+    }
+}
